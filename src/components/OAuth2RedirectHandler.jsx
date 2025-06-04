2c3b1aa3c710bc8b1382f6598c97da61b5533cc0
nohssam <nohssam73@gmail.com>
2025-06-04 13:05:59 +0900

    kakao 로그인, 로그아웃 성공 2 -주석달기기

--- src/components/OAuth2RedirectHandler.jsx
+++ src/components/OAuth2RedirectHandler.jsx
@@ -8,9 +8,16 @@ export default function OAuth2RedirectHandler(){
 
     useEffect(()=>{
         // 1.쿠키에서 authToken 확인
+        // document.cookie : 현재 웹사이트의 쿠키 문자열을 가져옴
         const token = document.cookie
+        // 쿠키를 배열로 나눕니다.
         .split("; ")
+        // 배열 중 "snsProvider="로 시작하는 항목을 찾습니다.
         .find((row)=> row.startsWith("authToken="))
+       // null 또는 undefined면 에러가 발생 방지 (?.)
+       // "snsProvider=kakao"를 "=" 기준으로 나눕니다 
+       //  → ["snsProvider", "kakao"]
+       // [1] 로 "kakao" 값만 가져옵니다.
         ?.split("=")[1];
         console.log(token);
 
